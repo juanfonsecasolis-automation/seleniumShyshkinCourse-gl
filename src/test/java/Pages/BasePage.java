6ca7b32bf0145494eef80f3871518954e0bb3036
Juan Fonseca <juanma2268@gmail.com>
2021-10-10 17:38:39 -0600

    Implemented Page Factory

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -1,13 +1,25 @@
 package Pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
 
-public class BasePage {
+public abstract class BasePage {
 	
-	WebDriver driver;
+	protected By anchorLocator; 
+	protected WebDriver driver;
+	final int TIMEOUT_PAGELOAD_SECS = 15;
 	
 	public BasePage(WebDriver driver) {
 		this.driver = driver;
+	}
+	
+	protected void initElements(Object childPage, By anchorLocator) {
+		PageFactory.initElements(driver, childPage);
+		new WebDriverWait(driver, TIMEOUT_PAGELOAD_SECS)
+			.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(anchorLocator));
 	}
 	
 }
--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -3,43 +3,37 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage {
-
-	//@FindBy(id="user-name")
+	
+	@FindBy(id="user-name")
 	WebElement usernameField;
 	
-	//@FindBy(id="password")
+	@FindBy(id="password")
 	WebElement passwordField;
 	
-	//@FindBy(id="login-button")
+	@FindBy(id="login-button")
 	WebElement loginButton;
 	
-	//@FindBy(css="html body div#root div div.login_wrapper div.login_wrapper-inner div#login_button_container.form_column div.login-box form div.error-message-container.error")
+	@FindBy(css="html body div#root div div.login_wrapper div.login_wrapper-inner div#login_button_container.form_column div.login-box form div.error-message-container.error")
 	WebElement errorDiv;
 	By errorDivLocator = By.cssSelector(".error-message-container > h3");
 	
 	public LoginPage(WebDriver driver) {
 		super(driver);
 		driver.navigate().to("https://www.saucedemo.com/");
+		initElements(this, By.cssSelector(".login_logo"));
 	}
 
 	public void login(String username, String password) {
-		/*usernameField.sendKeys(username);
+		usernameField.sendKeys(username);
 		passwordField.sendKeys(password);
-		loginButton.click();*/
-		driver.findElement(By.id("user-name")).sendKeys(username);
-		driver.findElement(By.id("password")).sendKeys(password);
-		driver.findElement(By.id("login-button")).click();
+		loginButton.click();
 	}
 
 	public String getErrorMessage() {
-		//return errorDiv.getText();
-		WebDriverWait wait = new WebDriverWait(driver, 15);
-		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(errorDivLocator));
-		return driver.findElement(errorDivLocator).getText();
+		return errorDiv.getText();
 	}
 	
 }
