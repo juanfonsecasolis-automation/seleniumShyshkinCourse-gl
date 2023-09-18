823b41e9691f60e8352fb6637f724df14a7d1bb9
juanfonsecasolis-automation <81777451+juanfonsecasolis-automation@users.noreply.github.com>
2023-09-17 18:19:42 -0600

    Update DriverFactory.java

--- src/test/java/Utilities/DriverFactory.java
+++ src/test/java/Utilities/DriverFactory.java
@@ -14,14 +14,14 @@ public class DriverFactory {
 		logger.info(String.format("Initializing driver for %s...", browser));
 		String[] options = new String[]{"--headless"};
 		if(browser.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
+			//System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
 			ChromeOptions options1 = new ChromeOptions();
 			for(String option : options) {
 				options1.addArguments(option);
 			}
 			driver = new ChromeDriver(options1);
 		}else if(browser.equals("firefox")) {
-				System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver");
+				//System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver");
 				FirefoxOptions options2 = new FirefoxOptions();
 				for(String option : options) {
 					options2.addArguments(option);
